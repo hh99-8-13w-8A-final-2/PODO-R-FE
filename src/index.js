91dc38c90494d4d74dcaaa7792101949645e5479
kksltv123 <kksltv123@naver.com>
2022-08-30 16:10:00 +0900

    [feat] setting common components

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
+import theme from './assets/style/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider>
+  <ThemeProvider theme={theme}>
     <App />
   </ThemeProvider>
 );
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,18 @@
-
 import React from 'react';
+import Header from '../components/common/Header';
+import Layout from '../components/common/Layout';
+import Footer from '../components/common/Footer';
+
 
 const MainPage = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Header/>
+            <Layout>
+
+            </Layout>
+            <Footer/>
+        </>
     );
 };
 
