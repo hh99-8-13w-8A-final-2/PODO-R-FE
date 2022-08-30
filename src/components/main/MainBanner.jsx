88e0883daa40cb213d774699fd2604a83e94458e
kksltv123 <kksltv123@naver.com>
2022-08-30 22:13:35 +0900

    [feat] banner

--- src/components/main/MainBanner.jsx
+++ src/components/main/MainBanner.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+import bnr from '../../assets/img/bnr.png'
 
 const MainBanner = () => {
     return (
-        <StMainBanner>
-
+        <StMainBanner bnr={bnr}>
+            <div>
+                <h3>포도알 서비스 런칭</h3>
+                <p>당신이 원하는 바로 그 자리 포도알에 전부 다 있습니다.</p>
+            </div>
         </StMainBanner>
     );
 };
 
 const StMainBanner = styled.div`
-    background-color: var(--gray-1);
     height: 300px;
+    width: 1400px;
+    background-image: ${props => `url(${props.bnr})`};
+    background-size: cover;
+    background-position: center;
+    color: var(--white);
+    position: relative;
+    div {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        left: 20px;
+    }
 `
 
 export default MainBanner;
