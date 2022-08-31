b0c6ec95f285bfe0f794bf4667f6d9574b6ff68f
kksltv123 <kksltv123@naver.com>
2022-08-31 20:07:13 +0900

    [test] create

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,7 +4,7 @@ import Layout from '../components/common/Layout';
 import Footer from '../components/common/Footer';
 import MainBanner from '../components/main/MainBanner';
 import TicketOpenList from '../components/main/TicketOpenList';
-import LiveReview from '../components/main/LiveReview';
+import LiveReviewList from '../components/main/LiveReviewList';
 import NoticeList from '../components/main/NoticeList';
 
 const MainPage = () => {
@@ -14,7 +14,7 @@ const MainPage = () => {
             <Layout>
                 <MainBanner/>
                 <TicketOpenList/>
-                <LiveReview/>
+                <LiveReviewList/>
                 <NoticeList/>
             </Layout>
             <Footer/>
