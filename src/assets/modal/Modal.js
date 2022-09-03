0d66bfd0dbb609b27e842cee0c08bba0a58f2ecc
ParkYongWon <dyddnjs1209@naver.com>
2022-09-03 14:53:39 +0900

    feet modal

--- src/assets/modal/Modal.js
+++ src/assets/modal/Modal.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
 
-const Modal = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Modal = ({ onClose, children }) => {
+  return (
+    <Background>
+      <Content>
+        {children}
+        <button onClick={onClose}>닫기</button>
+      </Content>
+    </Background>
+  );
 };
 
 export default Modal;
+
+
+const Background = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  left: 0;
+  top: 0;
+  text-align: center;
+  background: rgba(0, 0, 0, 0.9);
+  /* z-Index: auto; */
+`;
+
+const Content = styled.div`
+  height: 500px;
+  width: 500px;
+  position: relative;
+  /* overflow: scroll; */
+  background: pink;
+
+  /* z-Index: 1001; */
+`;
