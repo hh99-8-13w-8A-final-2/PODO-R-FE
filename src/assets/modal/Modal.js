cf5f97358abd2d73775969d2976f1216f9110880
kksltv123 <kksltv123@naver.com>
2022-09-08 00:03:03 +0900

    [test]test

--- src/assets/modal/Modal.js
+++ src/assets/modal/Modal.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from "styled-components";
 
-const Modal = () => {
-    return (
-        <div>
-        
-        </div>
-    );
+const Modal = ({ onClose}) => {
+
+  return (
+    <>
+    </>
+  );
 };
+
+
 
 export default Modal;
