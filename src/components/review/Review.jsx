12c728e40b6e239ac212c25b39c59aaaba11b028
kksltv123 <kksltv123@naver.com>
2022-10-06 17:13:33 +0900

    코드 정리

--- src/components/review/Review.jsx
+++ src/components/review/Review.jsx
@@ -12,7 +12,7 @@ import { ReactComponent as Like } from '../../assets/img/like.svg'
 import { ReactComponent as Comment } from '../../assets/img/comment.svg'
 import { useInView } from "react-intersection-observer";
 import moment from 'moment'
-import { WindowScroller, CellMeasurer, CellMeasurerCache, AutoSizer, List, InfiniteLoader } from "react-virtualized";
+import { WindowScroller, CellMeasurer, CellMeasurerCache, AutoSizer, List } from "react-virtualized";
 import { useState } from 'react';
 
 const cache = new CellMeasurerCache({
@@ -187,87 +187,6 @@ const Review = ({ handleModal, tagUrl }) => {
                     </AutoSizer>
                 )}
             </WindowScroller>
-            {/* {data?.pages.map((group, i) => {
-                return (
-                    <StWrap key={i}>
-                        <Fragment>
-                            {group.data.map((data) => {
-                                const changeToDate = (datetime) => {
-                                    // 오늘 날짜
-                                    let now = moment(new Date())
-                                    // 오늘과의 시간 차이
-                                    let duration = moment.duration(now.diff(datetime))
-                                    // 변환
-                                    // asSeconds 를 하면 오늘과의 시간차이를 
-                                    // 초단위로 float datatype 으로 보여준다 (3.82 이런식)
-                                    let seconds = duration.asSeconds()
-                                    let minute = duration.asMinutes()
-                                    let hours = duration.asHours()
-                                    let days = duration.asDays()
-                                    let weeks = duration.asWeeks()
-                                    let month = duration.asMonths()
-                                    let year = duration.asYears()
-                                    
-                                    // 그래서 사용할 때는 parseInt 를 사용해 int 로 바꿔야 한다. 
-                                    if (minute < 1) {
-                                        // 1분 미만이면 초 단위로 보여주고,  
-                                    return '방금 전'
-                                    } else if (hours < 1) {
-                                    // 1시간 미만이면 분 단위로 보여주고
-                                    return parseInt(minute) + '분 전'
-                                    } else if (hours < 24) {
-                                    // 하루 미만이면 시간으로 보여주고
-                                    return parseInt(hours) + '시간 전'
-                                    } else if (weeks < 1) {
-                                    // 일주일 미만이면 일 단위로 보여주고
-                                    return parseInt(days) + '일 전'
-                                    } else if (month < 1) {
-                                    // 한 달 미만이면 주 단위로 보여주고
-                                    return parseInt(weeks) + '주 전'
-                                    } else if (year < 1) {
-                                    // 1년 미만이면 달 단위로 보여주고
-                                    return parseInt(month) + '달 전'
-                                    } else {
-                                    // 1년 이상이면 넌 단위로 보여주고
-                                    return parseInt(year) + '년 전'
-                                    }
-                                }
-                                return (
-                                    <StReviewDiv key={data.reviewId} onClick={() => handleModal(data.reviewId, data.musicalId)}>
-                                        <StThumbDiv imgUrl={data.imgUrl}>
-                                            <StUtillDiv>
-                                                {data.heartChecked ? <><Like fill='#BB63FF' /><span>{data.heartCount}</span></> : <><Like fill='#fff' /><span>{data.heartCount}</span></>}
-                                                <Comment fill='#fff' /><span>{data.commentCount}</span>
-                                            </StUtillDiv>
-                                        </StThumbDiv>
-                                        <StInfoBox>
-                                            <StH3>{data.grade}석 {data.floor} {data.section !== "0" && <>{data.section}구역</>} {data.row}열 {data.seat}</StH3>
-                                            <StDate>
-                                                {changeToDate(data.createdAt)}
-                                            </StDate>
-                                            <StIconDiv>
-                                                {data.evaluation.gap === 3 && <div><Gap fill='#BB63FF' /><span>단차좋음</span></div>}
-                                                {data.evaluation.gap === 2 && <div><Gap fill='#444' /><span>단차보통</span></div>}
-                                                {data.evaluation.gap === 1 && <div><Gap fill='#444' /><span>단차나쁨</span></div>}
-                                                {data.evaluation.sight === 3 && <div><View fill='#BB63FF' /><span>시야좋음</span></div>}
-                                                {data.evaluation.sight === 2 && <div><View fill='#444' /><span>시야보통</span></div>}
-                                                {data.evaluation.sight === 1 && <div><View fill='#444' /><span>시야나쁨</span></div>}
-                                                {data.evaluation.sound === 3 && <div><Sound fill='#BB63FF' /><span>음향좋음</span></div>}
-                                                {data.evaluation.sound === 2 && <div><Sound fill='#444' /><span>음향보통</span></div>}
-                                                {data.evaluation.sound === 1 && <div><Sound fill='#444' /><span>음향나쁨</span></div>}
-                                                {data.evaluation.light === 3 && <div><Light fill='#BB63FF' /><span>조명좋음</span></div>}
-                                                {data.evaluation.light === 2 && <div><Light fill='#444' /><span>조명보통</span></div>}
-                                                {data.evaluation.light === 1 && <div><Light fill='#444' /><span>조명나쁨</span></div>}
-                                            </StIconDiv>
-                                        </StInfoBox>
-                                    </StReviewDiv>
-                                
-                                )
-                            })}
-                        </Fragment>
-                    </StWrap>
-                )
-            })} */}
             {render ? 
             <StMoreDiv
                 ref={ref}
